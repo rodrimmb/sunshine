31fb16c4c8c117a5e718cf0f07e74d05c90647aa
rodrimmb <rodrimmb@gmail.com>
2017-06-29 19:23:04 +0200

    Fragment con listado (Ejercicios tema 1)

--- app/src/main/java/es/rodrimmb/sunshine/MainActivity.java
+++ app/src/main/java/es/rodrimmb/sunshine/MainActivity.java
@@ -1,7 +1,16 @@
 package es.rodrimmb.sunshine;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            List<String> weekForecast = Arrays.asList(
+                    "Today - Sunny - 88/63",
+                    "Tomorrow - Foggy - 70/46",
+                    "Weds - Cloudy - 72/63",
+                    "Thurs - Sunny - 64/51",
+                    "Fri - Rain - 70/46",
+                    "Sat - Sunny - 76/68"
+            );
+            ArrayAdapter<String> forecastAdapter = new ArrayAdapter<String>(
+                    getActivity(),
+                    R.layout.list_item_forecast,
+                    R.id.list_item_forecast_textview,
+                    weekForecast
+            );
+
+            ListView forecastList = (ListView) rootView.findViewById(R.id.listview_forecast);
+            forecastList.setAdapter(forecastAdapter);
+
+            return rootView;
+        }
     }
 }
